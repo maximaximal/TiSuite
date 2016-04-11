a07cd85adb2db9207245c646dcede146a552a32e
Max Heisinger <mail@maximaximal.com>
2016-04-11 15:02:00 +0200

    Fix a build typo.

--- build_from_jenkins.sh
+++ build_from_jenkins.sh
@@ -5,12 +5,12 @@
 # TiC
 # ======
 
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "mkdir -p /root/jenkins/workspace/TiSuite/build/tic"
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "rm -f /root/jenkins/workspace/TiSuite/build/tic/tic-*"
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tic/ && cmake ../../TiC"
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tic/ && make"
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tic/ && make install"
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tic/ && make package"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "mkdir -p /root/jenkins/workspace/TiSuite/build/tic"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "rm -f /root/jenkins/workspace/TiSuite/build/tic/tic-*"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tic/ && cmake ../../TiC"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tic/ && make"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tic/ && make install"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tic/ && make package"
 
 echo "Add the package to the repository"
 DEB="$(ls /var/lib/jenkins/workspace/TiSuite/build/tic/*deb)"
@@ -20,11 +20,11 @@ cd /media/maximaximal.com/www/repos/apt/ubuntu && sudo reprepro includedeb trust
 # TiEdit
 # =======
 
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "mkdir -p /root/jenkins/workspace/TiSuite/build/tiedit"
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "rm -f /root/jenkins/workspace/TiSuite/build/tiedit/tic-*"
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tiedit/ && cmake ../../TiEdit"
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tiedit/ && make"
-sudo chroot /opt/chroots/ubuntu_trusty_x64/bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tiedit/ && make package"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "mkdir -p /root/jenkins/workspace/TiSuite/build/tiedit"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "rm -f /root/jenkins/workspace/TiSuite/build/tiedit/tic-*"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tiedit/ && cmake ../../TiEdit"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tiedit/ && make"
+sudo chroot /opt/chroots/ubuntu_trusty_x64 /bin/bash -c "cd /root/jenkins/workspace/TiSuite/build/tiedit/ && make package"
 
 echo "Add the package to the repository"
 DEB="$(ls /var/lib/jenkins/workspace/TiSuite/build/tic/*deb)"
